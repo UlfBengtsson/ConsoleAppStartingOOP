9f02c485b4a0397e6c5934ecd9439e42d7319170
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2022-02-21 11:33:38 +0100

    Add project files.

---
+++ ConsoleAppStartingOOP/Model/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppStartingOOP.Model
+{
+    internal class Person
+    {
+        //defualt access to methods/fields is private
+
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("First name must contain at least 2 letters.");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("First name must contain at least 2 letters.");
+                }
+                lastName = value;
+            }
+        }
+
+
+        //by defualt, if i dont make a cunstructor, C# will make one
+    }
+}
---
+++ ConsoleAppStartingOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppStartingOOP.Model;
+using System;
+
+namespace ConsoleAppStartingOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "Ulf";
+            person.LastName = "Bengtsson";
+
+            //Console.WriteLine(person);
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+}
